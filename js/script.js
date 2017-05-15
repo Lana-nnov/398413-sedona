98b809c2b638d5788aecf562bf97620afe5a2fbb
Lana-nnov <lana_nnov@mail.ru>
2017-05-15 16:25:08 +0300

    изменен скрипт для формы

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 	var link = document. querySelector (".search-feedback" );
 	var popap = document. querySelector(".feedback-form" );
-	var close = document. querySelector(".feedback-form-close" );	
+	
+	popap.classList.add("feedback-form-close");
 	
 	link.addEventListener("click", function (evt) { 
-	evt.preventDefault( );
-	popap.classList.add("feedback-form-close");
+	evt.preventDefault( );	
 	popap.classList.toggle("feedback-form-open"); });
