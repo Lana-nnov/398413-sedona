a77bbde8d3319c9bdbf12fd0e87c8287184916fb
Lana-nnov <lana_nnov@mail.ru>
2017-05-15 16:11:51 +0300

    исправлено JS для формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 	var link = document. querySelector (".search-feedback" );
 	var popap = document. querySelector(".feedback-form" );
-
+	var close = document. querySelector(".feedback-form-close" );	
+	
 	link.addEventListener("click", function (evt) { 
 	evt.preventDefault( );
-	popap.classList.toggle("feedback-form-close"); });
+	popap.classList.add("feedback-form-close");
+	popap.classList.toggle("feedback-form-open"); });
